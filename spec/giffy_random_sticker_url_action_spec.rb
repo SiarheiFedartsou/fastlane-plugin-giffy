4eaa093376106af47bb9b8d1ae9ee3f8a8479e0c
Oleksii Kuchma <nod3pad@gmail.com>
2017-05-04 22:23:41 +0300

    Fix validation for input parameter `original_url`

--- spec/giffy_random_sticker_url_action_spec.rb
+++ spec/giffy_random_sticker_url_action_spec.rb
@@ -6,12 +6,23 @@ describe Fastlane::Actions::GiffyRandomStickerUrlAction do
     end
 
     it 'returns URL to downsampled random sticker' do
-      url = Fastlane::Actions::GiffyRandomStickerUrlAction.run(api_key: 'dc6zaTOxFJmzC', tag: 'cats')
+      url = Fastlane::FastFile.new.parse("lane :test do
+          giffy_random_sticker_url ({
+            api_key: 'dc6zaTOxFJmzC',
+            tag: 'cats'
+          })
+        end").runner.execute(:test)
       expect(url).to eq('http://media1.giphy.com/media/l3N1m3sGa5K6s/200w_d.gif')
     end
 
     it 'returns URL to original random sticker' do
-      url = Fastlane::Actions::GiffyRandomStickerUrlAction.run(api_key: 'dc6zaTOxFJmzC', tag: 'cats', original_gif: true)
+      url = Fastlane::FastFile.new.parse("lane :test do
+          giffy_random_sticker_url ({
+            api_key: 'dc6zaTOxFJmzC',
+            tag: 'cats',
+            original_gif: true
+          })
+        end").runner.execute(:test)
       expect(url).to eq('http://media1.giphy.com/media/l3N1m3sGa5K6s/giphy.gif')
     end
   end
